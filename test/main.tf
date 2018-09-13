f0e1fb579a3512612da6a9d802bdb8a0a1377085
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2018-09-13 11:30:53 +0100

    Correct hard coded values and change policy statements for describe security groups

--- test/main.tf
+++ test/main.tf
@@ -2,6 +2,6 @@ module "csw_role" {
   source            = "../csw_role"
   prefix            = "${var.prefix}"
   region            = "${var.region}"
-  agent_account_id  = "103495720024"
-  target_account_id = "779799343306"
+  agent_account_id  = "${var.agent_account_id}"
+  target_account_id = "${var.target_account_id}"
 }
