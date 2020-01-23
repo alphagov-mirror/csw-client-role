2351e06efaac88a8defa6a7011253027aba532d0
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2020-01-23 15:29:13 +0000

    bump to terraform 0.12
    This upgrades the module to terraform 0.12.
    
    Terraform 0.12 has been out for a while.  Continuing to use tf 0.11
    syntax in here causes deprecation warnings in consumers, such as:
    
    ```
    Warning: Interpolation-only expressions are deprecated
    
      on .terraform/modules/csw_role/provider.tf line 2, in provider "aws":
       2:   region = "${var.region}"
    ```
    
    This commit upgrades to tf 0.12, mostly by running the handy
    `terraform 0.12upgrade` command.
    
    Consumers should pin to a commit hash so this shouldn't break existing
    users.

--- output.tf
+++ output.tf
@@ -1,11 +1,12 @@
 output "csw_role_id" {
-  value = "${module.csw_role.role_id}"
+  value = module.csw_role.role_id
 }
 
 output "csw_role_arn" {
-  value = "${module.csw_role.role_arn}"
+  value = module.csw_role.role_arn
 }
 
 output "csw_policy_id" {
-  value = "${module.csw_role.policy_id}"
+  value = module.csw_role.policy_id
 }
+
