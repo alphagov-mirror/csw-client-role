6ee39b44d0d58bfbe25d309223288e724e615e1d
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2020-01-23 16:25:18 +0000

    bump nested module to 0.12
    Oops, missed this the first time.  Terraform doesn't automatically
    upgrade nested modules and this took me by suprise.

--- csw_role/output.tf
+++ csw_role/output.tf
@@ -1,11 +1,12 @@
 output "role_id" {
-  value = "${aws_iam_role.cst_security_inspector_role.id}"
+  value = aws_iam_role.cst_security_inspector_role.id
 }
 
 output "role_arn" {
-  value = "${aws_iam_role.cst_security_inspector_role.arn}"
+  value = aws_iam_role.cst_security_inspector_role.arn
 }
 
 output "policy_id" {
-  value = "${aws_iam_role_policy.cst_security_inspector_role_policy.id}"
+  value = aws_iam_role_policy.cst_security_inspector_role_policy.id
 }
+
--- csw_role/policy.tf
+++ csw_role/policy.tf
@@ -1,17 +1,19 @@
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+}
 
 data "template_file" "policy" {
-  template = "${file("${path.module}/json/policy.json")}"
+  template = file("${path.module}/json/policy.json")
 
-  vars {
-    prefix     = "${var.prefix}"
-    account_id = "${data.aws_caller_identity.current.account_id}"
+  vars = {
+    prefix     = var.prefix
+    account_id = data.aws_caller_identity.current.account_id
   }
 }
 
 resource "aws_iam_role_policy" "cst_security_inspector_role_policy" {
   name = "${var.prefix}_CstSecurityInspectorRolePolicy"
-  role = "${aws_iam_role.cst_security_inspector_role.id}"
+  role = aws_iam_role.cst_security_inspector_role.id
 
-  policy = "${data.template_file.policy.rendered}"
+  policy = data.template_file.policy.rendered
 }
+
